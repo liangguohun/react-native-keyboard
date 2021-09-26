e326239020deef6c054b5ebbc25d765521642d21
liangguohun <liangguohun@qq.com>
2021-09-26 19:57:59 +0800

    keyboard android 端

--- README.md
+++ README.md
@@ -15,12 +15,10 @@ import Keyboard from "react-native-keyboard";
 
 // ...
 
-const result = await Keyboard.multiply(3, 7);
+Keyboard.hide()
 ```
 
 ## Contributing
-
-See the [contributing guide](CONTRIBUTING.md) to learn how to contribute to the repository and the development workflow.
 
 ## License
 
--- android/src/main/java/com/reactnativekeyboard/KeyboardModule.java
+++ android/src/main/java/com/reactnativekeyboard/KeyboardModule.java
@@ -3,12 +3,27 @@
 package com.reactnativekeyboard;
 
 import android.content.Context;
+import android.view.View;
 import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
 
 import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.uimanager.NativeViewHierarchyManager;
+import com.facebook.react.uimanager.UIImplementation;
+import com.facebook.react.uimanager.UIManagerModule;
+import com.facebook.react.uimanager.UIViewOperationQueue;
 
+import java.lang.reflect.Field;
+
+/**
+ * 参考：
+ * https://www.cnblogs.com/plokmju/p/7978500.html
+ * https://blog.csdn.net/qq_15602635/article/details/83751333
+ * https://blog.csdn.net/cym492224103/article/details/106375114
+ */
 public class KeyboardModule extends ReactContextBaseJavaModule {
 
     private final ReactApplicationContext reactContext;
@@ -32,8 +47,59 @@ public class KeyboardModule extends ReactContextBaseJavaModule {
     public void toggleKeyBord() {
         InputMethodManager imm = (InputMethodManager) reactContext.getSystemService(Context.INPUT_METHOD_SERVICE);
         if (imm != null) {
-            imm.toggleSoftInput(0, 0);
+            imm.toggleSoftInput(1, 0);
         }
     }
+
+    @ReactMethod
+    public void show() {
+      InputMethodManager imm = (InputMethodManager) reactContext.getSystemService(Context.INPUT_METHOD_SERVICE);
+      if (imm != null) {
+//        View view = getViewByTag(this.reactContext, viewTag);
+        View view = this.reactContext.getCurrentActivity().getWindow().getDecorView();
+        imm.showSoftInput(view, 1);
+      }
+    }
+
+    @ReactMethod
+    public void hide() {
+      InputMethodManager imm = (InputMethodManager) reactContext.getSystemService(Context.INPUT_METHOD_SERVICE);
+      if (imm != null) {
+//        View view = getViewByTag(this.reactContext, viewTag);
+        View view = this.reactContext.getCurrentActivity().getWindow().getDecorView();
+        imm.hideSoftInputFromWindow(view.getWindowToken(),0);
+      }
+    }
+
+//  public static View getViewByTag(ReactContext reactContext, int viewTag) {
+//    NativeViewHierarchyManager manager = getNativeViewHierarchyManager(reactContext);
+//    if (manager == null) {
+//      return null;
+//    }
+//    return manager.resolveView(viewTag);
+//  }
+//
+//  public static NativeViewHierarchyManager getNativeViewHierarchyManager(ReactContext reactContext) {
+//    try {
+//// 获取 UIImplementation
+//      UIManagerModule uiManager = reactContext.getNativeModule(UIManagerModule.class);
+//      UIImplementation uiImplementation = uiManager.getUIImplementation();
+//// 获取 UIImplementation#mOperationsQueue
+//      Field mOperationsQueueField = uiImplementation.getClass().getDeclaredField("mOperationsQueue");
+//      mOperationsQueueField.setAccessible(true);
+//      UIViewOperationQueue uiViewOperationQueue = (UIViewOperationQueue) mOperationsQueueField.get(uiImplementation);
+//// 获取 UIViewOperationQueue#NativeViewHierarchyManager
+//      Field mNativeViewHierarchyManagerField = UIViewOperationQueue.class.getDeclaredField("mNativeViewHierarchyManager");
+//      mNativeViewHierarchyManagerField.setAccessible(true);
+//      NativeViewHierarchyManager mNativeViewHierarchyManager = (NativeViewHierarchyManager) mNativeViewHierarchyManagerField.get(uiViewOperationQueue);
+//      return mNativeViewHierarchyManager;
+//    } catch (NoSuchFieldException e) {
+//      e.printStackTrace();
+//      return null;
+//    } catch (IllegalAccessException e) {
+//      e.printStackTrace();
+//      return null;
+//    }
+//  }
 
 }
